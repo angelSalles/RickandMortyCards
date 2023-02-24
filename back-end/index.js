ca6fa02ba1292d7c5733cb869e805ba1c01d4639
Angel Sales <80469180+angelSalles@users.noreply.github.com>
2023-02-24 09:08:47 -0400

    AAA

--- back-end/index.js
+++ back-end/index.js
@@ -13,7 +13,7 @@ async function main(){
     const collection = db.collection("controleTI");
     console.log("Conectado");
     const app = express();
-    const port = 3000;
+    const port = process.env.PORT || 3000;
 
     app.use(express.json());
 
